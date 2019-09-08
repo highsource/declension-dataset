8f69c7cfde80d6111a4bcc9bcc3b3617072cbfd2
Alexey Valikov <aleksei.valikov@gmail.com>
2019-09-08 13:14:23 +0200

    Importing the dataset into a DynamoDB table.

---
+++ dynamodb-import/index.js
@@ -0,0 +1,20 @@
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'eu-west-1'});
+var dataset = require("./dataset");
+console.log("Total number of entries:" + dataset.length);
+
+var documentClient = new AWS.DynamoDB.DocumentClient(/*{convertEmptyValues:true}*/);
+
+dataset.forEach(entry => {
+	var params =  {
+  		TableName: "DeutscheDeklinationInflection",
+		Item: entry
+	};
+	documentClient.put(params, function(err, data) {
+		if (err) {
+			console.log("Error", err);
+		} else {
+			console.log("Processed: " + entry.word);
+		}
+	});
+});
